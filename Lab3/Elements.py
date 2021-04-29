9ac696c310be8a22c6507c56d0b584d20ce1c431
karrick99 <riccardo.carita7@gmail.com>
2021-04-29 18:33:36 +0200

    update

--- Lab3/Elements.py
+++ Lab3/Elements.py
@@ -1,22 +1,22 @@
 #
 
 class Signal_information:
-    def __init__(self, signal_power, path = []):
+    def __init__(self, signal_power : float):
         self.signal_power = signal_power
         self.noise_power = 0.0
         self.latency = 0.0
         self.path = []
 
-    def updatePwr(self, signalPwrIncr):
-        self.signal_power = self.signal_power + signalPwrIncr
+    def update_pwr(self, signal_pwr_incr):
+        self.signal_power = self.signal_power + signal_pwr_incr
 
-    def updateNoise(self, noisePwrIncr):
-        self.noise_power = self.noise_power + noisePwrIncr
+    def update_noise(self, noise_pwr_incr):
+        self.noise_power = self.noise_power + noise_pwr_incr
 
-    def updateLatency(self, latencyIncr):
-        self.latency = self.latency + latencyIncr
+    def update_latency(self, latency_incr):
+        self.latency = self.latency + latency_incr
 
-    def updatePath(self, newPath):
+    def update_path(self):
         self.path.pop(0)
 
 
@@ -28,14 +28,28 @@ class Node :
             self.connected_nodes = dict['connected_nodes']
             self.successive = {}
 
-    def propagate(self, signalInfObj):
-        signalInfObj.updatePath()
-        path = signalInfObj.path[0]
+    def propagate(self, signal_inf_obj):
 
-        #here I have to refer to the next node:
-        path.propagate()
+        line_lable = signal_inf_obj.path[0]+signal_inf_obj.path[1]
+        line = self.successive[line_lable]
+        signal_inf_obj.update_path()
+        line.propagate(signal_inf_obj)
+
 
 
 class Line:
     def __init__(self):
+        self.label = []
+        self.length = 0.0
+        self.successive = {}
 
+    def latency_generation(self, signal_inf_obj):
+        signal_inf_obj.update_latency()
+        return float
+
+
+
+class Network:
+    def __init__(self):
+        nodes = {Node}
+        lines = {Line}
