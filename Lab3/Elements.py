32aab01db059a3a02a1266e0b09ecb1bd649537d
karrick99 <riccardo.carita7@gmail.com>
2021-05-08 16:01:35 +0200

    Tested - Network creation

--- Lab3/Elements.py
+++ Lab3/Elements.py
@@ -1,11 +1,14 @@
 #
+import json
+import math
+
 
 class Signal_information:
-    def __init__(self, signal_power : float):
+    def __init__(self, signal_power, path):
         self.signal_power = signal_power
         self.noise_power = 0.0
         self.latency = 0.0
-        self.path = []
+        self.path = path[:]
 
     def update_pwr(self, signal_pwr_incr):
         self.signal_power = self.signal_power + signal_pwr_incr
@@ -19,37 +22,72 @@ class Signal_information:
     def update_path(self):
         self.path.pop(0)
 
+    def get_path(self):
+        return self.path
 
 
-class Node :
-    def __init__(self, dict):
-            self.label = dict['label']
-            self.position = dict['position']
-            self.connected_nodes = dict['connected_nodes']
-            self.successive = {}
+class Node:
+    def __init__(self, key, d):
+        self.label = key
+        self.position = d['position'][:]
+        self.connected_nodes = d['connected_nodes'][:]
+        self.successive = {}
 
     def propagate(self, signal_inf_obj):
+        """propagates the signal to the end of the path"""
 
-        line_lable = signal_inf_obj.path[0]+signal_inf_obj.path[1]
-        line = self.successive[line_lable]
+        line_lable = signal_inf_obj.path[0] + signal_inf_obj.path[1]
         signal_inf_obj.update_path()
-        line.propagate(signal_inf_obj)
+        next_element = self.successive[line_lable]
+        next_element.propagate(signal_inf_obj)
 
+    def get_position(self):
+        return self.position
+
+    def get_connected_nodes(self):
+        return self.connected_nodes
 
 
 class Line:
-    def __init__(self):
-        self.label = []
-        self.length = 0.0
+    """Connection between two nodes"""
+
+    def __init__(self, label, length):
+        # potrebbe servire costruttore per assegnare length e lable
+        self.label = label
+        self.length = length
         self.successive = {}
 
-    def latency_generation(self, signal_inf_obj):
-        signal_inf_obj.update_latency()
-        return float
+    def latency_generation(self):
+        t = self.length / ((5 * 10 ** 5) * 2 / 3)
+        return t
 
+    def noise_generation(self, signal_power):
+        n = signal_power * self.length * 1e-9
+        return n
+
+    def propagate(self, signal_inf_obj):
+        signal_inf_obj.update_latency(self.latency_generation())
+        signal_inf_obj.update_noise(self.noise_generation(signal_inf_obj.signal_power))
+        # signal_inf_obj.get_path[1]
 
 
 class Network:
     def __init__(self):
-        nodes = {Node}
-        lines = {Line}
+        self.nodes = {}
+        self.lines = {}
+
+        with open("nodes.json", "r") as in_file:
+            data = json.load(in_file)
+
+        for key in data:
+            self.nodes[key] = Node(key, data[key])
+
+        for key in self.nodes:
+            for target in self.nodes[key].get_connected_nodes():
+                self.lines[key + target] = Line(key + target,
+                                                self.__distance(self.nodes[key].get_position(),
+                                                                self.nodes[target].get_position()))
+
+    def __distance(self, pos1, pos2):
+        dist = math.sqrt((pos2[0] - pos1[0]) ** 2 + (pos2[1] - pos1[1]) ** 2)
+        return dist
---
+++ main.py
@@ -0,0 +1,4 @@
+import Lab3.Elements as elems
+
+N = elems.Network()
+print(dir(N))
